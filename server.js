9394ffd699a4e5debde54651dbfb9747be474b92
Daniel Simão <rui.daniel.simao@gmail.com>
2019-06-19 15:34:29 +0100

    SPA

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ mongoose
 
 app.use("/api/posts", require("./routes/api/posts"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/public/"));
+
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on porto ${port}`));
